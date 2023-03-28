29063d24b4f8d33f96cdfe8ebe6323a055296334
ywalakamar <barxwells@gmail.com>
2023-03-28 14:58:33 +0300

    feature(customer):ui customer registration

--- script.js
+++ script.js
@@ -8,3 +8,53 @@ registerLink.addEventListener("click", () => {
 loginLink.addEventListener("click", () => {
   logregBox.classList.remove("active");
 });
+
+const userSignUp = async () => {
+  var url = "http://localhost:8000/customers/signup";
+  var addressurl = "http://localhost:8000/customers/address";
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  var phone = document.getElementById("phone").value;
+  var street = document.getElementById("street").value;
+  var city = document.getElementById("city").value;
+  var postalCode = document.getElementById("postal-code").value;
+
+  var country = document.getElementById("country").value;
+
+  try {
+    const signupresponse = await fetch(url, {
+      method: "POST",
+      headers: {
+        mode: "no-cors",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password, phone }),
+    });
+
+    const {
+      data: { token },
+    } = await signupresponse.json();
+
+    const addressResponse = await fetch(addressurl, {
+      method: "POST",
+      headers: {
+        mode: "no-cors",
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+
+      body: JSON.stringify({ street, postalCode, city, country }),
+    });
+
+    const { success } = await addressResponse.json();
+    if (success) {
+      window.location.href = "index.html";
+    }
+  } catch (error) {
+    return error;
+  }
+};
+
+document.getElementById("signUp").addEventListener("click", userSignUp);
